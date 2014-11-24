664b99d2e09b3c64d68a52cc91fb0df79ec0ea70
Artem Zinoviev <arzinoviev@gmail.com>
2014-11-24 18:18:23 +0300

    change for readability

--- js/timer.js
+++ js/timer.js
@@ -324,7 +324,9 @@ var digitalTimer = {
                 }
             }
 
-            dd.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00") + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00") + ":" + (seconds > 9 ? seconds : "0" + seconds);
+            dd.textContent = (hours ? (hours > 9 ? hours : "0" + hours) : "00")
+                + ":" + (minutes ? (minutes > 9 ? minutes : "0" + minutes) : "00")
+                + ":" + (seconds > 9 ? seconds : "0" + seconds);
 
             timer();
         }
