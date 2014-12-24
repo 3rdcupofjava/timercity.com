df3bbf515c5645af313f81fbc30a2a05e72d4a59
3rdcupofjava <3rdcupofjava@gmail.com>
2014-12-24 11:31:38 -0500

    simple change

--- js/storage.js
+++ js/storage.js
@@ -1,29 +1,29 @@
-function storage_check(){
-    // IF RETURNING USER
-    if(localStorage.getItem('clock_s') !== null) {
-
-    } else if(localStorage.getItem('timerClock_s') !== null) {
-
-    } else if(localStorage.getItem('countDownClock_s') !== null) {
-
-    } else if(localStorage.getItem('stopWatchClock_s') !== null) {
-
-    } else if(localStorage.getItem('lapTimerClock_s') !== null) {
-
-    } else {
-
-        timerClock.render(130, 0, 'Demo Timer'); // timer presented if user came in first time and no global \ local storage.
-    }
-
-}
+//function storage_check(){
+//    // IF RETURNING USER
+//    if(localStorage.getItem('clock_s') !== null) {
+//
+//    } else if(localStorage.getItem('timerClock_s') !== null) {
+//
+//    } else if(localStorage.getItem('countDownClock_s') !== null) {
+//
+//    } else if(localStorage.getItem('stopWatchClock_s') !== null) {
+//
+//    } else if(localStorage.getItem('lapTimerClock_s') !== null) {
+//
+//    } else {
+//
+//        timerClock.render(130, 0, 'Demo Timer'); // timer presented if user came in first time and no global \ local storage.
+//    }
+//
+//}
 
 var storage = {
     local : {
         save : function() {
-            alert('local save');
+            localStorage.setItem($('#storage_key_load').val(), JSON.stringify(timerClock_s));
         },
         load : function() {
-            alert('local load');
+            localStorage.getItem($('#storage_key_load').val());
         }
     },
     global : {
